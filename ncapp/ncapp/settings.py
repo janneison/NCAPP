1827338fbd88cd9bdf252612e15525ecab3e10a2
Janneison Galindo <janneisongalindo@gmail.com>
2017-11-16 12:39:02 -0500

    configuro db

--- ncapp/ncapp/settings.py
+++ ncapp/ncapp/settings.py
@@ -115,10 +115,11 @@ LOCALE_PATHS = (
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }""" 
-DATABASES = { 'default': dj_database_url.config() }
+
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = { 'default': dj_database_url.config() }
 DATABASES['default'].update(db_from_env)
 
 
