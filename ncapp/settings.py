4cc29860fd1e52c3db61eaf708ddf00c4254daf4
Janneison Galindo <janneisongalindo@gmail.com>
2017-11-22 19:06:24 -0500

    cambio settings

--- ncapp/settings.py
+++ ncapp/settings.py
@@ -152,12 +152,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
