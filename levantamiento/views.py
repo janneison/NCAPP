7aaa59845848c9b4c32363f4e5da4bff62a340c0
Janneison Galindo <janneisongalindo@gmail.com>
2017-12-04 17:13:52 -0500

    cambios para informe

--- levantamiento/views.py
+++ levantamiento/views.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+""" 
 # Create your views here.
+from docx import Document
+from docx.shared import Inches , Pt
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.style import WD_STYLE
+from docx.enum.text import WD_ALIGN_PARAGRAPH , WD_COLOR_INDEX
+from rest_framework.request import Request
+
+from levantamiento.models import (Proyecto, Convenio, 
+Circuito, Apoyo, Version, TipoDato, Capitulo, 
+Levantamiento, Estado, DetalleLevantamiento,SoporteNC,
+Norma, Defecto, Actividad)
+
+@login_required
+def generar_informe(request):
+    document = Document()
+    paragraph = document.add_paragraph('Informe de Calidad.')
+
+    document.add_heading('Calidad de las obras')
+
+    document.add_paragraph('A continuación, se presentan los hallazgos representativos de calidad hallados en las inspecciones de calidad:')
+
+
+    table = document.add_table(rows=2, cols=2)
+
+    document.save('informe/test.docx')
+""" 
--- ncapp/resource.py
+++ ncapp/resource.py
@@ -12,3 +12,6 @@ MessageNC = {
     'vacio': 'No se encontraron datos', 
     'errorServidor': 'Se presentaron errores de comunicacion con el servidor'
 }
+
+meses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+dias=['31','28','31','30','31','30','31','31','30','31','30','31']
